91abe9cf8d89b7794ae7ec696c2a699086bd4bd0
Shama <assiddik001@gmail.com>
2022-05-23 23:53:44 +0600

    DB connect

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // Use Middleware
 app.use(cors());
 app.use(express.json());
+
+// Connect DB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7gyhq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+  console.log("db connect");
+});
 
 app.get("/", (req, res) => {
   res.send("Electric Cart is running");
