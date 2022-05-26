46aea3d08874e12a68fd1a87eb15e5ed43a1fa7f
Shama <assiddik001@gmail.com>
2022-05-26 15:42:14 +0600

    verifyAdmin middleware

--- index.js
+++ index.js
@@ -44,6 +44,20 @@ async function run() {
     const orderCollection = client.db("electricCart").collection("orders");
     const userCollection = client.db("electricCart").collection("users");
     const reviewCollection = client.db("electricCart").collection("reviews");
+
+    // Admin varify
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        next();
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
+    };
+
     //  Get all products
     app.get("/product", async (req, res) => {
       const query = {};
@@ -126,22 +140,14 @@ async function run() {
     });
 
     // Make admin
-    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+    app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
-      const requester = req.decoded.email;
-      const requesterAccount = await userCollection.findOne({
-        email: requester,
-      });
-      if (requesterAccount.role === "admin") {
-        const filter = { email: email };
-        const updateDoc = {
-          $set: { role: "admin" },
-        };
-        const result = await userCollection.updateOne(filter, updateDoc);
-        res.send(result);
-      } else {
-        res.status(403).send({ message: "forbidden" });
-      }
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
     });
 
     // Gap
